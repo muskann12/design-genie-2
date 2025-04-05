b5569607bff7d78ce372fcea8c5cad2fff864f1a
muskann12 <nisarmuskan14@gmail.com>
2025-04-05 21:35:01 +0500

    initial push to new repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopHeader from "./components/topheader";
+import Header from "./components/navbar";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopHeader />
+        <Header />
+
         {children}
+        <Footer />
       </body>
     </html>
   );
